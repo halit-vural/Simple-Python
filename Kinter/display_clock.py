60fb24dd3644616299c7f0a0056b756854b6d6ba
Halit Vural <97833535+hvbosna@users.noreply.github.com>
2022-04-01 13:06:16 +0300

    Create display_clock.py

---
+++ Kinter/display_clock.py
@@ -0,0 +1,29 @@
+from sqlite3 import Timestamp
+from tkinter import *
+import time
+
+def myTime():
+    timeStr = time.strftime('%I:%M:%S %p')
+    dateStr = time.strftime('%A')
+    
+    timeLabel.config(text=timeStr)
+    dateLabel.config(text=dateStr)
+    dateLabel.after(1000, myTime)
+    timeLabel.after(1000, myTime)
+
+
+myWin = Tk()
+myWin.title('Digital Clock Display')
+myWin.geometry('600x200')   #width x height
+
+timeLabel = Label(myWin, font=('Arial', 72), fg='white', bg='black')
+timeLabel.pack()
+
+dateLabel = Label(myWin, font=('Arial', 50), fg='white', bg='black')
+dateLabel.pack()
+
+myTime()
+
+    
+
+myWin.mainloop()
