e49df27b5d09724836d8071223d871b25dbb33d0
Halit Vural <97833535+hvbosna@users.noreply.github.com>
2022-09-30 21:54:21 +0000

    FirstBricks start

---
+++ FirstBricks/1_first_program.py
@@ -0,0 +1,10 @@
+# This program says hello and asks for my name.
+
+print('Hello, world!')
+print('What is your name?')    # ask for their name
+myName = input()
+print('It is good to meet you, ' + myName)
+
+print('What is your age?')    # ask for their age
+myAge = input()
+print('I think your birth year is ', 2022-myAge, '?')
